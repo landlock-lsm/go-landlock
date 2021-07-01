1131915d2f77395a38507fa297e9e76b0b96e683
Günther Noack <guenther@unix-ag.uni-kl.de>
2021-07-01 22:53:04 +0200

    A basic test
    If the local Linux installation is missing Landlock support, the test
    can be compiled using `go test -v .` and the resulting binary executed
    using `./golandlock.test -test.v` from a different Linux installation.

---
+++ restrict_test.go
@@ -0,0 +1,30 @@
+package golandlock_test
+
+import (
+	"errors"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/gnoack/golandlock"
+)
+
+// Make sure that after landlocking, the password file can't be read any more.
+// XXX: Landlocking in the test itself makes it difficult to compose.
+func TestAccessingPasswordFile(t *testing.T) {
+	_, err := os.ReadFile("/etc/passwd")
+	if err != nil {
+		t.Skipf("expected normal accesses to /etc/passwd to work, got error: %v", err)
+	}
+
+	roDirs := []string{"/tmp"}
+	err = golandlock.Restrict(roDirs, nil, nil, nil)
+	if errors.Is(err, syscall.ENOSYS) {
+		t.Skipf("kernel does not support Landlock; tests cannot be run: %v", err)
+	}
+
+	_, err = os.ReadFile("/etc/passwd")
+	if !errors.Is(err, syscall.EACCES) {
+		t.Errorf("expected that bar/a can't be read, got error: %v", err)
+	}
+}
