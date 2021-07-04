7a381de4dcdb5feb32013ed649e466f54f8d8b97
Günther Noack <guenther@unix-ag.uni-kl.de>
2021-07-04 22:59:32 +0200

    Add a simple smoke test script.
    The script is using the command line tool and assumes that it is
    available pre-built in the current directory. (You can create it with
    `go build cmd/landlock-restrict/main.go`)

---
+++ tests/test.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+# The golandlock tests are currently just a shell script. Running
+# Landlock from within a regular Go test is harder to manage, as tests
+# can interfere with each other.
+
+enter () {
+    printf "[Test] %60s" "$*"
+}
+
+success () {
+    echo " [OK]"
+}
+
+fail () {
+    echo " [FAIL] "
+    echo
+    echo "****************"
+    echo $*
+    echo "****************"
+    echo
+    echo "Direcory contents:"
+    find .
+    echo
+    echo "Stdout:"
+    cat stdout.txt
+    echo
+    echo "Stderr:"
+    cat stderr.txt
+    exit 1
+}
+
+shutdown() {
+    rm -rf "${TMPDIR}"
+}
+
+expect_success() {
+    if [ "$?" -ne 0 ]; then
+        fail "Expected:" $*
+    fi
+    success
+}
+
+expect_failure() {
+    if [ "$?" -eq 0 ]; then
+        fail "Expected:" $*
+    fi
+    success
+}
+
+# Run
+run() {
+    "${CMD}" -v -ro /bin /usr $* >stdout.txt 2>stderr.txt
+}
+
+CMD="$(pwd)/main"
+
+if [ ! -f "${CMD}" ]; then
+    echo "Sandboxing command does not exist: ${CMD}"
+    echo "Cannot run the tests."
+    exit 1
+fi
+
+TMPDIR=$(mktemp -t -d golandlock-test.XXXXXX)
+echo "Running in ${TMPDIR}"
+cd "${TMPDIR}"
+trap shutdown EXIT
+
+# Set up an initial environment:
+mkdir -p foo
+echo lolcat > foo/lolcat.txt
+
+# Tests
+enter "No sandboxing, read works"
+/bin/cat foo/lolcat.txt > /dev/null
+expect_success "reading file should have worked"
+
+enter "No permissions, doing nothing succeeds"
+run -- /bin/true
+expect_success "doing nothing should succeed"
+
+enter "No permissions, read fails"
+run -- /bin/cat foo/lolcat.txt
+expect_failure "should have failed to read file"
+
+enter "Read permissions on dir (relative path), read works"
+run -ro "foo" -- /bin/cat foo/lolcat.txt
+expect_success "should have read the file"
+
+enter "Read permissions on dir (full path), read works"
+run -ro "${TMPDIR}/foo" -- /bin/cat foo/lolcat.txt
+expect_success "should have read the file"
+
+# XXX: Find out why this fails! Needs non-directory flags?
+# enter "Read permissions on file, read works"
+# run -ro "foo/lolcat.txt" -- /bin/cat foo/lolcat.txt
+# expect_success "should have read the file"
+
+enter "Read-only permissions on dir, creating file fails"
+run -ro "foo" -- /bin/touch foo/fail
+expect_failure "should not be able to create file"
+
+enter "RW permissions on dir, creating file succeeds"
+run -rw "foo" -- /bin/touch foo/succeed
+expect_success "should be able to create file"
+
+enter "Read-only permissions on dir, removing file fails"
+run -ro "foo" -- /bin/rm foo/succeed
+expect_failure "should not be able to remove file"
+
+enter "RW permissions on dir, removing file succeeds"
+run -rw "foo" -- /bin/rm foo/succeed
+expect_success "should be able to remove file"
+
+enter "Read-only permissions on dir, mkfifo fails"
+run -ro "foo" -- /bin/mkfifo foo/fifo
+expect_failure "should not be able to create file"
+
+enter "RW permissions on dir, mkfifo succeeds"
+run -rw "foo" -- /bin/mkfifo foo/fifo
+expect_success "should be able to create file"
+rm foo/fifo
+
+echo
+echo "All tests executed."
