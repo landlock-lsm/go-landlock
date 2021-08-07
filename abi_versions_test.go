4ff4d2eebfc61225305495811caeb37b5a53921f
Günther Noack <guenther@unix-ag.uni-kl.de>
2021-08-07 23:05:16 +0200

    ABI versions test

---
+++ abi_versions_test.go
@@ -0,0 +1,24 @@
+package golandlock
+
+import (
+	"testing"
+
+	ll "github.com/gnoack/golandlock/syscall"
+)
+
+func TestAbiVersionsIncrementing(t *testing.T) {
+	for i, abiInfo := range abiInfos {
+		if i != abiInfo.version {
+			t.Errorf("Expected ABI version %d at index %d, got version %d", i, i, abiInfo.version)
+		}
+	}
+}
+
+func TestSupportedAccessFS(t *testing.T) {
+	got := abiInfos[1].supportedAccessFS
+	want := uint64(ll.AccessFSWriteFile | ll.AccessFSRemoveDir | ll.AccessFSRemoveFile | ll.AccessFSMakeChar | ll.AccessFSMakeDir | ll.AccessFSMakeReg | ll.AccessFSMakeSock | ll.AccessFSMakeFifo | ll.AccessFSMakeBlock | ll.AccessFSMakeSym | ll.AccessFSExecute | ll.AccessFSReadFile | ll.AccessFSReadDir)
+
+	if got != want {
+		t.Errorf("V1 supported access FS: got %x, want %x", got, want)
+	}
+}
