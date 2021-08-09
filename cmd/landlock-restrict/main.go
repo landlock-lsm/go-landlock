3a791d5aa72c8569a2ca9a78fe5692b74bfe6ccb
Günther Noack <guenther@unix-ag.uni-kl.de>
2021-08-09 20:08:04 +0200

    landlock-restrict: add -rofiles, -rwfiles flags

--- cmd/landlock-restrict/main.go
+++ cmd/landlock-restrict/main.go
@@ -10,7 +10,15 @@ import (
 	"github.com/gnoack/golandlock"
 )
 
-func parseFlags(args []string) (verbose bool, ro, rw, cmd []string) {
+func takeArgs(args, out []string) (argsOut, outOut []string) {
+	for len(args) > 0 && !strings.HasPrefix(args[0], "-") {
+		out = append(out, args[0])
+		args = args[1:]
+	}
+	return args, out
+}
+
+func parseFlags(args []string) (verbose bool, roDirs, rwDirs, roFiles, rwFiles, cmd []string) {
 	for len(args) > 0 {
 		switch args[0] {
 		case "-v":
@@ -19,43 +27,55 @@ func parseFlags(args []string) (verbose bool, ro, rw, cmd []string) {
 			continue
 		case "-ro":
 			args = args[1:]
-			for len(args) > 0 && !strings.HasPrefix(args[0], "-") {
-				ro = append(ro, args[0])
-				args = args[1:]
-			}
+			args, roDirs = takeArgs(args, roDirs)
 			continue
 		case "-rw":
 			args = args[1:]
-			for len(args) > 0 && !strings.HasPrefix(args[0], "-") {
-				rw = append(rw, args[0])
-				args = args[1:]
-			}
+			args, rwDirs = takeArgs(args, rwDirs)
+			continue
+		case "-rofiles":
+			args = args[1:]
+			args, roFiles = takeArgs(args, roFiles)
+			continue
+		case "-rwfiles":
+			args = args[1:]
+			args, rwFiles = takeArgs(args, rwFiles)
 			continue
 		case "--":
 			args = args[1:]
 			// Remaining args are the command
 			cmd = args
-			return verbose, ro, rw, cmd
+			return verbose, roDirs, rwDirs, roFiles, rwFiles, cmd
 		default:
 			log.Fatalf("Unrecognized option %q", args[0])
 		}
 	}
 
 	cmd = args
-	return verbose, ro, rw, cmd
+	return verbose, roDirs, rwDirs, roFiles, rwFiles, cmd
 }
 
 func main() {
-	verbose, roPaths, rwPaths, cmdArgs := parseFlags(os.Args[1:])
+	verbose, roDirs, rwDirs, roFiles, rwFiles, cmdArgs := parseFlags(os.Args[1:])
 	if verbose {
 		fmt.Println("Landlock restricting to:")
-		fmt.Printf("  RO dirs: %v\n", roPaths)
-		fmt.Printf("  RW dirs: %v\n", rwPaths)
+		fmt.Printf("  RO dirs : %v\n", roDirs)
+		fmt.Printf("  RW dirs : %v\n", rwDirs)
+		fmt.Printf("  RO files: %v\n", roFiles)
+		fmt.Printf("  RW files: %v\n", rwFiles)
 		fmt.Println()
 		fmt.Printf("Executing command %v\n", cmdArgs)
 	}
 
 	if len(cmdArgs) < 1 {
+		fmt.Println("Usage:")
+		fmt.Println("  landlock-restrict [-ro PATH...] [-rw PATH...] [-rofiles PATH] [-rwfiles PATH] -- COMMAND...")
+		fmt.Println()
+		fmt.Println("Options:")
+		fmt.Println("  -ro, -rw, -rofiles, -rwfiles   paths to restrict to")
+		fmt.Println("  -verbose                       verbose logging")
+		fmt.Println()
+
 		log.Fatalf("Need proper command, got %v", cmdArgs)
 	}
 
@@ -64,8 +84,10 @@ func main() {
 	}
 
 	err := golandlock.V1.BestEffort().RestrictPaths(
-		golandlock.RODirs(roPaths...),
-		golandlock.RWDirs(rwPaths...),
+		golandlock.RODirs(roDirs...),
+		golandlock.RWDirs(rwDirs...),
+		golandlock.ROFiles(roFiles...),
+		golandlock.RWFiles(rwFiles...),
 	)
 	if err != nil {
 		log.Fatalf("landlock: %v", err)
