f4c0488a9d573c0d69fc21cd1a7cdae42d6f7c78
Günther Noack <guenther@unix-ag.uni-kl.de>
2022-06-05 16:56:14 +0200

    Add command line utility to read the Landlock ABI version

---
+++ cmd/landlock-abi-version/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	ll "github.com/landlock-lsm/go-landlock/landlock/syscall"
+)
+
+func main() {
+	v, err := ll.LandlockGetABIVersion()
+	if err != nil {
+		fmt.Println("0")
+	}
+	fmt.Println(v)
+}
