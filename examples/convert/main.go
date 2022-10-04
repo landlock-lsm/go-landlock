f5b03a1c9b892f8cb8f8d8852c4ce1124fc9e8ec
Günther Noack <gnoack3000@gmail.com>
2022-10-04 21:06:38 +0200

    examples: Image converter

---
+++ examples/convert/main.go
@@ -0,0 +1,37 @@
+// Command convert implements a landlocked image converter.
+//
+// Usage:
+// ./convert < input.jpeg > output.png
+//
+// This is a basic command line utility that reads from stdin and
+// writes to stdout. It has no business opening any additional files,
+// so we forbid it with a Landlock policy. Security issues in media
+// parsing libraries should not let the attacker access the file
+// system.
+package main
+
+import (
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+	"log"
+	"os"
+
+	"github.com/landlock-lsm/go-landlock/landlock"
+)
+
+func main() {
+	if err := landlock.V2.BestEffort().RestrictPaths(); err != nil {
+		log.Fatal("Could not enable seccomp:", err)
+	}
+
+	imgData, _, err := image.Decode(os.Stdin)
+	if err != nil {
+		log.Fatal("Could not read input:", err)
+	}
+
+	if err := png.Encode(os.Stdout, imgData); err != nil {
+		log.Fatal("Could not write output:", err)
+	}
+}
