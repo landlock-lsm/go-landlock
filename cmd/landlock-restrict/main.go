landlock-lsm/go-landlock

commit 75108384f76f2a4cf6bca21cea9cc65306a4443c

Landlock ABI v6 support (scoped IPC)
This feature restricts the use of UNIX signals and dialing out to
Linux's Abstract UNIX Domain Sockets when the recipient process is in
a more privileged or unrelated Landlock domain.  When the restriction
is enabled, these forms of IPC are only permitted if the target
process is in the same or a less privileged (nested) Landlock domain.

Using ABI v6 and higher with Go-Landlock requires that the Landlock
erratum 2 (fix for signal scoping) is fixed.  On kernels without this
bugfix, the feature is otherwise too incompatible with the inherently
multithreaded Go runtime.

Fixes: #35
Fixes: #36
Link: https://lore.kernel.org/all/cover.1725657727.git.fahimitahera@gmail.com/
Link: https://lore.kernel.org/all/cover.1725494372.git.fahimitahera@gmail.com/
Signed-off-by: Günther Noack <gnoack3000@gmail.com>

cmd/landlock-restrict/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -11,7 +12,8 @@ import (
 )
 
 func parseFlags(args []string) (verbose bool, cfg landlock.Config, opts []landlock.Rule, cmd []string) {
-	cfg = landlock.V5
+	configs := []landlock.Config{landlock.V1, landlock.V2, landlock.V3, landlock.V4, landlock.V5, landlock.V6}
+	cfg = configs[len(configs)-1]
 
 	takeArgs := func(makeOpt func(...string) landlock.FSRule) landlock.Rule {
 		var paths []string
@@ -48,26 +50,12 @@ func parseFlags(args []string) (verbose bool, cfg landlock.Config, opts []landlo
 ArgParsing:
 	for len(args) > 0 {
 		switch args[0] {
-		case "-5":
-			cfg = landlock.V5
-			args = args[1:]
-			continue
-		case "-4":
-			cfg = landlock.V4
-			args = args[1:]
-			continue
-		case "-3":
-			cfg = landlock.V3
-			args = args[1:]
-			continue
-		case "-2":
-			cfg = landlock.V2
-			args = args[1:]
-			continue
-		case "-1":
-			cfg = landlock.V1
-			args = args[1:]
-			continue
+		case "-1", "-2", "-3", "-4", "-5", "-6":
+			v, err := strconv.Atoi(args[0][1:])
+			if err != nil {
+				log.Fatal(err)
+			}
+			cfg = configs[v-1]
 		case "-strict":
 			bestEffort = false
 			args = args[1:]
@@ -122,14 +110,14 @@ func main() {
 		fmt.Println("Usage:")
 		fmt.Println("  landlock-restrict")
 		fmt.Println("     [-v]")
-		fmt.Println("     [-1] [-2] [-3] [-4] [-5] [-strict]")
+		fmt.Println("     [-1] [-2] [-3] [-4] [-5] [-6] [-strict]")
 		fmt.Println("     [-ro [+refer] PATH...] [-rw [+refer] [+ioctl_dev] PATH...]")
 		fmt.Println("     [-rofiles [+refer] PATH] [-rwfiles [+refer] PATH]")
 		fmt.Println("       -- COMMAND...")
 		fmt.Println()
 		fmt.Println("Options:")
 		fmt.Println("  -ro, -rw, -rofiles, -rwfiles   paths to restrict to")
-		fmt.Println("  -1, -2, -3, -4, -5             select Landlock version")
+		fmt.Println("  -1, -2, -3, -4, -5, -6         select Landlock version")
 		fmt.Println("  -strict                        use strict mode (instead of best effort)")
 		fmt.Println("  -v                             verbose logging")
 		fmt.Println()

cmd/landlock-restrict-scoped/main.go
@@ -1,0 +1,48 @@
+// landlock-restrict-scoped executes a process with Landlock scope restrictions
+//
+// This is an example tool which does not provide backwards
+// compatibility guarantees.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+
+	"github.com/landlock-lsm/go-landlock/landlock"
+)
+
+func usage() {
+	var (
+		out  = flag.CommandLine.Output()
+		name = os.Args[0]
+	)
+	fmt.Fprintf(out, "Usage of %s:\n", name)
+	flag.PrintDefaults()
+	fmt.Fprintf(out, "\nExample usages:\n")
+	fmt.Fprintf(out, "  %s -- /usr/bin/kill -USR1 $$\n", name)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	var cmd []string
+	if flag.NArg() > 1 {
+		cmd = flag.Args()
+	} else {
+		log.Println("missing command to call, using /bin/bash")
+		cmd = []string{"/bin/bash"}
+	}
+
+	if err := landlock.V6.RestrictScoped(); err != nil {
+		log.Fatalf("landlock RestrictScoped: %v", err)
+	}
+
+	log.Printf("Starting %v", cmd)
+	if err := syscall.Exec(cmd[0], cmd, os.Environ()); err != nil {
+		log.Fatalf("execve: %v", err)
+	}
+}

landlock/abi_versions.go
@@ -6,6 +6,7 @@ type abiInfo struct {
 	version            int
 	supportedAccessFS  AccessFSSet
 	supportedAccessNet AccessNetSet
+	supportedScoped    ScopedSet
 }
 
 var abiInfos = []abiInfo{
@@ -35,12 +36,19 @@ var abiInfos = []abiInfo{
 		supportedAccessFS:  (1 << 16) - 1,
 		supportedAccessNet: (1 << 2) - 1,
 	},
+	{
+		version:            6,
+		supportedAccessFS:  (1 << 16) - 1,
+		supportedAccessNet: (1 << 2) - 1,
+		supportedScoped:    (1 << 2) - 1,
+	},
 }
 
 func (a abiInfo) asConfig() Config {
 	return Config{
 		handledAccessFS:  a.supportedAccessFS,
 		handledAccessNet: a.supportedAccessNet,
+		scoped:           a.supportedScoped,
 	}
 }
 
@@ -56,6 +64,19 @@ func getSupportedABIVersion() abiInfo {
 	}
 	if v >= len(abiInfos) {
 		v = len(abiInfos) - 1
+	}
+	if v >= 6 {
+		// Check that the signal scoping bug is fixed,
+		// otherwise downgrade to v5.  This should happen only
+		// seldomly, as the bugfix was backported to newer
+		// versions of the 6.12 LTS kernel.
+		errata, err := ll.LandlockGetErrata()
+		if err != nil {
+			errata = 0 // pretend none fixed
+		}
+		if (errata & 0x2) != 0 {
+			v = 5
+		}
 	}
 	return abiInfos[v]
 }

landlock/config.go
@@ -39,6 +39,9 @@ var (
 	V4 = abiInfos[4].asConfig()
 	// Landlock V5 support (V4 + ioctl on device files)
 	V5 = abiInfos[5].asConfig()
+	// Landlock V6 support (V5 + IPC scopes for signals and
+	// Abstract Unix Domain Sockets (c.f. unix(7)))
+	V6 = abiInfos[6].asConfig()
 )
 
 // v0 denotes "no Landlock support". Only used internally.
@@ -50,17 +53,22 @@ var v0 = Config{}
 type Config struct {
 	handledAccessFS  AccessFSSet
 	handledAccessNet AccessNetSet
+	scoped           ScopedSet
 	bestEffort       bool
 }
 
 // NewConfig creates a new Landlock configuration with the given parameters.
 //
-// Passing an AccessFSSet will set that as the set of file system
-// operations to restrict when enabling Landlock. The AccessFSSet
-// needs to stay within the bounds of what go-landlock supports.
-// (If you are getting an error, you might need to upgrade to a newer
-// version of go-landlock.)
-func NewConfig(args ...interface{}) (*Config, error) {
+// Passing an AccessFSSet, AccessNetSet or ScopedSet will set these as
+// the set of file system/network/scoped operations to restrict when
+// enabling Landlock. The sets need to stay within the bounds of what
+// go-landlock supports.  (If you are getting an error, you might need
+// to upgrade to a newer version of go-landlock.)
+//
+// Example:
+//
+//	cfg, err := NewConfig(AccessFSSet(llsyscall.AccessFSExecute))
+func NewConfig(args ...any) (*Config, error) {
 	// Implementation note: This factory is written with future
 	// extensibility in mind. Only specific types are supported as
 	// input, but in the future more might be added.
@@ -86,6 +94,14 @@ func NewConfig(args ...interface{}) (*Config, error) {
 				return nil, errors.New("unsupported AccessNetSet value; upgrade go-landlock?")
 			}
 			c.handledAccessNet = arg
+		case ScopedSet:
+			if !c.scoped.isEmpty() {
+				return nil, errors.New("only one ScopedSet may be provided")
+			}
+			if !arg.valid() {
+				return nil, errors.New("unsupported ScopedSet value; upgrade go-landlock?")
+			}
+			c.scoped = arg
 		default:
 			return nil, fmt.Errorf("unknown argument %v; only AccessFSSet-type argument is supported", arg)
 		}
@@ -94,7 +110,7 @@ func NewConfig(args ...interface{}) (*Config, error) {
 }
 
 // MustConfig is like NewConfig but panics on error.
-func MustConfig(args ...interface{}) Config {
+func MustConfig(args ...any) Config {
 	c, err := NewConfig(args...)
 	if err != nil {
 		panic(err)
@@ -122,6 +138,11 @@ func (c Config) String() string {
 		fsDesc = "all"
 	}
 
+	var scopedDesc = c.scoped.String()
+	if abi.supportedScoped == c.scoped && c.scoped != 0 {
+		scopedDesc = "all"
+	}
+
 	var bestEffort = ""
 	if c.bestEffort {
 		bestEffort = " (best effort)"
@@ -134,7 +155,7 @@ func (c Config) String() string {
 		version = fmt.Sprintf("V%v", abi.version)
 	}
 
-	return fmt.Sprintf("{Landlock %v; FS: %v; Net: %v%v}", version, fsDesc, netDesc, bestEffort)
+	return fmt.Sprintf("{Landlock %v; FS: %v; Net: %v; Scoped: %v%v}", version, fsDesc, netDesc, scopedDesc, bestEffort)
 }
 
 // BestEffort returns a config that will opportunistically enforce
@@ -241,11 +262,15 @@ func (c Config) BestEffort() Config {
 //
 // [Kernel Documentation about Access Rights]: https://www.kernel.org/doc/html/latest/userspace-api/landlock.html#access-rights
 func (c Config) RestrictPaths(rules ...Rule) error {
-	c.handledAccessNet = 0 // clear out everything but file system access
+	// clear out everything but file system access
+	c = Config{
+		handledAccessFS: c.handledAccessFS,
+		bestEffort:      c.bestEffort,
+	}
 	return restrict(c, rules...)
 }
 
-// RestrictNet restricts network access in goroutines.
+// RestrictNet restricts network access in all goroutines.
 //
 // Using Landlock V4, this function will disallow the use of bind(2)
 // and connect(2) for TCP ports, unless those TCP ports are
@@ -259,8 +284,26 @@ func (c Config) RestrictPaths(rules ...Rule) error {
 //
 // [Kernel Documentation about Network flags]: https://www.kernel.org/doc/html/latest/userspace-api/landlock.html#network-flags
 func (c Config) RestrictNet(rules ...Rule) error {
-	c.handledAccessFS = 0 // clear out everything but network access
+	// clear out everything but network access
+	c = Config{
+		handledAccessNet: c.handledAccessNet,
+		bestEffort:       c.bestEffort,
+	}
 	return restrict(c, rules...)
+}
+
+// RestrictScoped restricts scoped IPC access in all goroutines.
+//
+// Starting with Landlock V6, this restricts the use of IPC mechanisms
+// like signals and abstract unix domain sockets, when talking to
+// processes in more privileged Landlock domains.
+func (c Config) RestrictScoped() error {
+	// clear out everything but scoped operations
+	c = Config{
+		scoped:     c.scoped,
+		bestEffort: c.bestEffort,
+	}
+	return restrict(c)
 }
 
 // Restrict restricts all types of access which is restrictable with the Config.
@@ -285,7 +328,8 @@ type PathOpt = Rule
 // compatibleWith is true if c is compatible to work at the given Landlock ABI level.
 func (c Config) compatibleWithABI(abi abiInfo) bool {
 	return (c.handledAccessFS.isSubset(abi.supportedAccessFS) &&
-		c.handledAccessNet.isSubset(abi.supportedAccessNet))
+		c.handledAccessNet.isSubset(abi.supportedAccessNet) &&
+		c.scoped.isSubset(abi.supportedScoped))
 }
 
 // restrictTo returns a config that is a subset of c and which is compatible with the given ABI.
@@ -293,6 +337,7 @@ func (c Config) restrictTo(abi abiInfo) Config {
 	return Config{
 		handledAccessFS:  c.handledAccessFS.intersect(abi.supportedAccessFS),
 		handledAccessNet: c.handledAccessNet.intersect(abi.supportedAccessNet),
+		scoped:           c.scoped.intersect(abi.supportedScoped),
 		bestEffort:       true,
 	}
 }

landlock/config_test.go
@@ -1,6 +1,7 @@
 package landlock
 
 import (
+	"fmt"
 	"testing"
 
 	ll "github.com/landlock-lsm/go-landlock/landlock/syscall"
@@ -13,27 +14,27 @@ func TestConfigString(t *testing.T) {
 	}{
 		{
 			cfg:  Config{handledAccessFS: 0, handledAccessNet: 0},
-			want: "{Landlock V0; FS: ∅; Net: ∅}",
+			want: "{Landlock V0; FS: ∅; Net: ∅; Scoped: ∅}",
 		},
 		{
 			cfg:  Config{handledAccessFS: ll.AccessFSWriteFile},
-			want: "{Landlock V1; FS: {write_file}; Net: ∅}",
+			want: "{Landlock V1; FS: {write_file}; Net: ∅; Scoped: ∅}",
 		},
 		{
 			cfg:  Config{handledAccessNet: ll.AccessNetBindTCP},
-			want: "{Landlock V4; FS: ∅; Net: {bind_tcp}}",
+			want: "{Landlock V4; FS: ∅; Net: {bind_tcp}; Scoped: ∅}",
 		},
 		{
 			cfg:  V1,
-			want: "{Landlock V1; FS: all; Net: ∅}",
+			want: "{Landlock V1; FS: all; Net: ∅; Scoped: ∅}",
 		},
 		{
 			cfg:  V1.BestEffort(),
-			want: "{Landlock V1; FS: all; Net: ∅ (best effort)}",
+			want: "{Landlock V1; FS: all; Net: ∅; Scoped: ∅ (best effort)}",
 		},
 		{
 			cfg:  Config{handledAccessFS: 1 << 63},
-			want: "{Landlock V???; FS: {1<<63}; Net: ∅}",
+			want: "{Landlock V???; FS: {1<<63}; Net: ∅; Scoped: ∅}",
 		},
 	} {
 		got := tc.cfg.String()
@@ -44,17 +45,54 @@ func TestConfigString(t *testing.T) {
 }
 
 func TestNewConfig(t *testing.T) {
-	for _, a := range []AccessFSSet{
-		ll.AccessFSWriteFile, ll.AccessFSRefer,
+	for _, tt := range []struct {
+		name string
+		args []any
+		want Config
+	}{
+		{
+			name: "fs_write_file",
+			args: []any{AccessFSSet(ll.AccessFSWriteFile)},
+			want: Config{handledAccessFS: ll.AccessFSWriteFile},
+		},
+		{
+			name: "fs_refer",
+			args: []any{AccessFSSet(ll.AccessFSRefer)},
+			want: Config{handledAccessFS: ll.AccessFSRefer},
+		},
+		{
+			name: "net_bind",
+			args: []any{AccessNetSet(ll.AccessNetBindTCP)},
+			want: Config{handledAccessNet: ll.AccessNetBindTCP},
+		},
+		{
+			name: "scoped_signal",
+			args: []any{ScopedSet(ll.ScopeSignal)},
+			want: Config{scoped: ll.ScopeSignal},
+		},
+		{
+			name: "christmas_tree",
+			args: []any{
+				AccessFSSet(ll.AccessFSReadDir | ll.AccessFSReadFile),
+				AccessNetSet(ll.AccessNetBindTCP | ll.AccessNetConnectTCP),
+				ScopedSet(ll.ScopeSignal | ll.ScopeAbstractUnixSocket),
+			},
+			want: Config{
+				handledAccessFS:  ll.AccessFSReadDir | ll.AccessFSReadFile,
+				handledAccessNet: ll.AccessNetBindTCP | ll.AccessNetConnectTCP,
+				scoped:           ll.ScopeSignal | ll.ScopeAbstractUnixSocket,
+			},
+		},
 	} {
-		c, err := NewConfig(a)
-		if err != nil {
-			t.Errorf("NewConfig(): expected success, got %v", err)
-		}
-		want := a
-		if c.handledAccessFS != want {
-			t.Errorf("c.handledAccessFS = %v, want %v", c.handledAccessFS, want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			cfg, err := NewConfig(tt.args...)
+			if err != nil {
+				t.Errorf("NewConfig(): expected success, got %v", err)
+			}
+			if *cfg != tt.want {
+				t.Errorf("cfg = %v, want %v", cfg, tt.want)
+			}
+		})
 	}
 }
 
@@ -86,5 +124,37 @@ func TestNewConfigFailures(t *testing.T) {
 		if err == nil {
 			t.Errorf("NewConfig(%v) success, expected error", args)
 		}
+	}
+}
+
+func TestCompatibleWithABI(t *testing.T) {
+	for i, abi := range abiInfos {
+		cfg := abi.asConfig()
+		t.Run(fmt.Sprintf("V%v", i), func(t *testing.T) {
+			for j := 0; j < i; j++ {
+				if cfg.compatibleWithABI(abiInfos[j]) {
+					t.Errorf("cfg.compatibleWithABI(abiInfos[%v]) = true, want false", j)
+				}
+			}
+			for j := i; j < len(abiInfos); j++ {
+				if !cfg.compatibleWithABI(abiInfos[j]) {
+					t.Errorf("cfg.compatibleWithABI(abiInfos[%v]) = false, want true", j)
+				}
+			}
+		})
+	}
+}
+
+func TestRestrictTo(t *testing.T) {
+	for i, abi := range abiInfos {
+		cfg := abi.asConfig()
+		t.Run(fmt.Sprintf("V%v", i), func(t *testing.T) {
+			for j := 0; j < len(abiInfos); j++ {
+				compatCfg := cfg.restrictTo(abiInfos[j])
+				if !compatCfg.compatibleWithABI(abiInfos[j]) {
+					t.Errorf("compatCfg.compatibleWithABI(abiInfos[%v]) = false, want true", j)
+				}
+			}
+		})
 	}
 }

landlock/landlock.go
@@ -5,12 +5,12 @@
 // The following invocation will restrict all goroutines so that they
 // can only read from /usr, /bin and /tmp, and only write to /tmp:
 //
-//	err := landlock.V5.BestEffort().RestrictPaths(
+//	err := landlock.V6.BestEffort().RestrictPaths(
 //	    landlock.RODirs("/usr", "/bin"),
 //	    landlock.RWDirs("/tmp"),
 //	)
 //
-// This will restrict file access using Landlock V5, if available. If
+// This will restrict file access using Landlock V6, if available. If
 // unavailable, it will attempt using earlier Landlock versions than
 // the one requested. If no Landlock version is available, it will
 // still succeed, without restricting file accesses.
@@ -20,20 +20,30 @@
 // The following invocation will restrict all goroutines so that they
 // can only bind to TCP port 8080 and only connect to TCP port 53:
 //
-//	err := landlock.V5.BestEffort().RestrictNet(
+//	err := landlock.V6.BestEffort().RestrictNet(
 //	    landlock.BindTCP(8080),
 //	    landlock.ConnectTCP(53),
 //	)
 //
-// This functionality is available since Landlock V5.
+// This functionality is available since Landlock V4.
 //
-// # Restricting file access and networking at once
+// # Restricting IPC scopes
 //
-// The following invocation restricts both file and network access at
-// once.  The effect is the same as calling [Config.RestrictPaths] and
-// [Config.RestrictNet] one after another, but it happens in one step.
+// The following invocation will restrict IPC to more privileged
+// Landlock domains, if possible:
 //
-//	err := landlock.V5.BestEffort().Restrict(
+//	err := landlock.V6.BestEffort().RestrictScoped()
+//
+// This functionality is available since Landlock V6.
+//
+// # Restricting all types of operations at once
+//
+// The following invocation restricts all types of operations at once.
+// The effect is the same as calling [Config.RestrictPaths],
+// [Config.RestrictNet] and [Config.RestrictScoped] one after another,
+// but it happens in one step.
+//
+//	err := landlock.V6.BestEffort().Restrict(
 //	    landlock.RODirs("/usr", "/bin"),
 //	    landlock.RWDirs("/tmp"),
 //	    landlock.BindTCP(8080),
@@ -42,9 +52,9 @@
 //
 // # More possible invocations
 //
-// landlock.V5.RestrictPaths(...) (without the call to
+// landlock.V6.RestrictPaths(...) (without the call to
 // [Config.BestEffort]) enforces the given rules using the
-// capabilities of Landlock V5, but returns an error if that
+// capabilities of Landlock V6, but returns an error if that
 // functionality is not available on the system that the program is
 // running on.
 //
@@ -73,7 +83,7 @@
 // possible, but it will ensure that all the requested access rights
 // are permitted after Landlock enforcement.
 //
-// # Current limitations
+// # Current limitations (File System)
 //
 // Landlock can not currently restrict all file system operations.
 // The operations that can and can not be restricted yet are listed in
@@ -91,6 +101,16 @@
 // These are Landlock limitations that will be resolved in future
 // versions. See the [Kernel Documentation about Current Limitations]
 // for more details.
+//
+// # Current Limitations (IPC within the same Go program)
+//
+// For a Go process which restricts Landlock "scoped" IPC operations
+// for itself, goroutines within this process might be inconsistently
+// permitted or denied to use these IPC mechanisms among themselves.
+//
+// This is because different OS threads within the Go process may
+// technically belong to different Landlock domains (even if these
+// domains enforce the same policies).
 //
 // # Multithreading Limitations
 //

landlock/lltest/lltest.go
@@ -13,16 +13,12 @@ import (
 	ll "github.com/landlock-lsm/go-landlock/landlock/syscall"
 )
 
-// isRunningInSubprocess indicates whether we are currently running in a subprocess context.
-var isRunningInSubprocess = false
-
 // RunInSubprocess runs the given test function in a subprocess
 // and forwards its output.
 func RunInSubprocess(t *testing.T, f func()) {
 	t.Helper()
 
-	if os.Getenv("IS_SUBPROCESS") != "" {
-		isRunningInSubprocess = true
+	if IsRunningInSubprocess() {
 		f()
 		return
 	}
@@ -67,7 +63,7 @@ func RunInSubprocess(t *testing.T, f func()) {
 func TempDir(t testing.TB) string {
 	t.Helper()
 
-	if isRunningInSubprocess {
+	if IsRunningInSubprocess() {
 		dir, err := os.MkdirTemp("", "LandlockTestTempDir")
 		if err != nil {
 			t.Fatalf("os.MkdirTemp: %v", err)
@@ -77,11 +73,34 @@ func TempDir(t testing.TB) string {
 	return t.TempDir()
 }
 
+func abiVersion() (int, error) {
+	// TODO(gnoack): This logic must stay in line with
+	// getSuppportedABIVersion().
+	v, err := ll.LandlockGetABIVersion()
+	if err != nil {
+		return 0, err
+	}
+	if v >= 6 {
+		errata, err := ll.LandlockGetErrata()
+		if err != nil {
+			return 0, err
+		}
+		if (errata & 0x2) != 0 {
+			v = 5
+		}
+	}
+	return v, nil
+}
+
 // RequireABI skips the test if the kernel does not provide the given ABI version.
 func RequireABI(t testing.TB, want int) {
 	t.Helper()
 
-	if v, err := ll.LandlockGetABIVersion(); err != nil || v < want {
+	if v, err := abiVersion(); err != nil || v < want {
 		t.Skipf("Requires Landlock >= V%v, got V%v (err=%v)", want, v, err)
 	}
+}
+
+func IsRunningInSubprocess() bool {
+	return os.Getenv("IS_SUBPROCESS") != ""
 }

landlock/restrict.go
@@ -53,13 +53,14 @@ func restrict(c Config, rules ...Rule) error {
 	// always implicit, even in Landlock V1. So enabling Landlock
 	// on a Landlock V1 kernel without any handled access rights
 	// will still forbid linking files between directories.
-	if c.handledAccessFS.isEmpty() && c.handledAccessNet.isEmpty() {
+	if c.handledAccessFS.isEmpty() && c.handledAccessNet.isEmpty() && c.scoped.isEmpty() {
 		return nil // Success: Nothing to restrict.
 	}
 
 	rulesetAttr := ll.RulesetAttr{
 		HandledAccessFS:  uint64(c.handledAccessFS),
 		HandledAccessNet: uint64(c.handledAccessNet),
+		Scoped:           uint64(c.scoped),
 	}
 	fd, err := ll.LandlockCreateRuleset(&rulesetAttr, 0)
 	if err != nil {

commit 8c15473541c4e489c82b5d4110b27ea572eb987c

landlock-restrict: Fix broken -l argument parsing
Fixes: 75108384f76f2a4cf6bca21cea9cc65306a4443c
Signed-off-by: Günther Noack <gnoack3000@gmail.com>

cmd/landlock-restrict/main.go
@@ -59,8 +59,11 @@ ArgParsing:
 				log.Fatal(err)
 			}
 			cfg = configs[v-1]
+			args = args[1:]
+			continue
 		case "-l":
 			cfg = cfg.EnableLoggingForSubprocesses()
+			args = args[1:]
 			continue
 		case "-strict":
 			bestEffort = false